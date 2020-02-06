e0d74c723b400552f250e3d9d20d55c490b6b322
Kélio Steiner <kepo.steiner@yahoo.fr>
2020-02-06 16:34:49 +0100

    Généralisation de la création de gameObjects

--- New Unity Project/Assets/CreatedContent/Scripts/GameObjectScript/FloorScript.cs
+++ New Unity Project/Assets/CreatedContent/Scripts/GameObjectScript/FloorScript.cs
@@ -2,18 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FloorClass : MonoBehaviour
+public class FloorScript : MonoBehaviour
 {
     public int FloorNumber { get; set; }
-    public GameObject Enemy { get; set; }  // Réaliser le GameObject de l'ennemi
+    public EnemyScript Enemy { get; set; }  // Réaliser le GameObject de l'ennemi
     public GameObject Container { get; set; }  // Réaliser le GameObject du coffre
 
-    // Constructeurs
-    public FloorClass() { }
-    public FloorClass(int floorNumber)
+    public void Initialize(int floorNumber)
     {
         this.FloorNumber = floorNumber;
 
+        this.Enemy = ObjectFactory.CreateEnemy("trucmuche", 50, 25);
         // TODO - Faire un script de génération pour un ennemi et un coffre
     }
 }
