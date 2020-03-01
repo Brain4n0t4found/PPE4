e4f9e0f48651e62e5788c0649dbf26399c9f2c9e
Brain not found <kepo.steiner@yahoo.fr>
2020-03-01 01:57:15 +0100

    Ajout fonction script personnage

--- New Unity Project/Assets/CreatedContent/Scripts/GameObjectScript/MainCharacterScript.cs
+++ New Unity Project/Assets/CreatedContent/Scripts/GameObjectScript/MainCharacterScript.cs
@@ -1,6 +1,8 @@
 ﻿// Using System
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 // Using Unity
 using UnityEngine;
@@ -56,5 +58,38 @@ public class MainCharacterScript : MonoBehaviour
             if (WeaponList.Count > 0)
                 EquippedWeapon = WeaponList[0];
         }
+    }
+
+    /// <summary>
+    /// Cherche et détruit une clé dans l'inventaire du personnage
+    /// </summary>
+    /// <returns>boolean si le personnage a une clé ou non</returns>
+    public bool TryUseKey()
+    {
+        // Tentative de récupération d'une clé dans la liste d'objets du personnage
+        EquipmentObjectClass potentialKey = EquipmentObjects.Any(obj => obj.Name == "Key")
+            ? EquipmentObjects.Where(obj => obj.Name == "Key").First()
+            : null;
+        
+        if (potentialKey != null)
+        {
+            Destroy(potentialKey.gameObject);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Renvoie un booléen selon si le personnage est fatigué ou non
+    /// </summary>
+    /// <returns>Boolean</returns>
+    public bool CanDoAction()
+    {
+        return CharacterStates.Any(stat => stat.Name == "Fatigué") 
+            ? false  // Si le personnage est affecté par l'état "fatigué"
+            : true;  // Sinon
     }
 }
