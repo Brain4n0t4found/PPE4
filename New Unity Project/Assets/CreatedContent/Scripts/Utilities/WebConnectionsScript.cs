4a60d5f87f1d96d68d9dce80375b56f471eec68f
Kélio Steiner <kepo.steiner@yahoo.fr>
2020-03-22 00:53:56 +0100

    Ajout de récupération de données depuis une BDD

---
+++ New Unity Project/Assets/CreatedContent/Scripts/Utilities/WebConnectionsScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class WebConnectionsScript : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(GetDBHighScores());
+    }
+
+    IEnumerator GetDBHighScores()
+    {
+        UnityWebRequest webRequest = UnityWebRequest.Get("https://portfolioks.fr/ConnexionToDB.php");
+
+        yield return webRequest.SendWebRequest();
+
+        if (webRequest.isNetworkError || webRequest.isHttpError)
+        {
+            Debug.Log(webRequest.error);
+        }
+        else
+        {
+            string scoresInformations = webRequest.downloadHandler.text;
+            Debug.Log(scoresInformations);
+        }
+    }
+}
